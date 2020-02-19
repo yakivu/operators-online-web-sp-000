442d1dcaa252dad54c71a8a02514266b587462d1
Adam Walker <adamw207@yahoo.com>
2020-02-19 15:14:06 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,14 @@
 def unsafe?(speed)
-!= [40 .to eq 60]
+if unsafe?(speed) < 40 : > 60
+  "unsafe"
+else
+  "safe"
 end
 
 
 
 def not_safe?(speed)
+	( not_safe?(speed) < 40 ? 'unsafe' : 'safe') : ( not_safe?(speed) > 60 ? 'unsafe' : 'safe')
 	
 end
 	

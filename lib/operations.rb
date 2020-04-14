902f756c13c22c9b4d3e53de1fc3bf081a2787dc
Adam Walker <adamw207@yahoo.com>
2020-04-14 18:12:34 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,19 @@
 def unsafe?(speed)
-if unsafe?(speed) < 40 : > 60
-  "unsafe"
-else
-  "safe"
-end
+  if speed > 60
+    true 
+    elsif speed < 40 
+    true 
+    else
+      false 
+      end
+    end
+   
 
 
 
 def not_safe?(speed)
-	( not_safe?(speed) < 40 ? 'unsafe' : 'safe') : ( not_safe?(speed) > 60 ? 'unsafe' : 'safe')
+	( not_safe?(speed) < 40 ? 'unsafe' : 'safe') ; ( not_safe?(speed) > 60 ? 'unsafe' : 'safe')
 	
 end
 	
-
 
